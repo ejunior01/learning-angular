328753ee5fbe178e9ce69d84af13231c16f90629
Edvan Ribeiro Dos Santos Junior <jk.jr47@gmail.com>
2025-02-19 00:57:02 +0000

    Add page component products

--- capputeeno/src/app/app.routes.ts
+++ capputeeno/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ProductsComponent } from './feature/catalog/pages/products/products.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+  path: '',
+  component: ProductsComponent,
+  title: 'Produtos'
+}];
