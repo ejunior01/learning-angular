03e1ee9781abff773bbed3cb7abe3c9090f71543
Edvan Ribeiro Dos Santos Junior <jk.jr47@gmail.com>
2025-03-21 00:35:45 -0300

    feat: adicionar modelo de curso e tabela de exibição no componente de cursos

--- crud-angular-spring/frontend/src/app/courses/courses/courses.component.ts
+++ crud-angular-spring/frontend/src/app/courses/courses/courses.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { Course } from '../models/course';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-courses',
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './courses.component.html',
-  styleUrl: './courses.component.scss'
+  styleUrl: './courses.component.scss',
 })
 export class CoursesComponent {
-
+  courses: Course[] = [
+    {
+      _id: '1',
+      name: 'Angular',
+      category: 'Front-end',
+    },
+  ];
+  displayedColumns = ['name', 'category'];
 }
